edd212675952e79d12e846fb9ed01a8c9bc0c8da
AbdussamadYisau <Sammieyisau@gmail.com>
2020-07-20 00:44:30 +0100

    Used environment variables to store API credentials

--- Project4-EvaluateNewsNlp/src/server/index.js
+++ Project4-EvaluateNewsNlp/src/server/index.js
@@ -1,6 +1,14 @@
-var path = require('path')
-const express = require('express')
-const mockAPIResponse = require('./mockAPI.js')
+var path = require('path');
+const express = require('express');
+const mockAPIResponse = require('./mockAPI.js');
+const aylienTextApi = require('aylien_textapi');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const textapi = new aylienTextApi({
+    application_id: process.env.API_ID,
+    application_key: process.env.API_KEY
+});
 
 const app = express()
 app.use(cors());
